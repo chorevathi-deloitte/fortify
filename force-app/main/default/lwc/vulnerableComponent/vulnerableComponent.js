444f87010da05e13abfa186711681cb09146fcaa
chorevathi-deloitte <chorevathi@deloitte.com>
2026-05-11 18:27:34 +0530

    Update vulnerableComponent.js

--- force-app/main/default/lwc/vulnerableComponent/vulnerableComponent.js
+++ force-app/main/default/lwc/vulnerableComponent/vulnerableComponent.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class VulnerableComponent extends LightningElement {
     userInput = '';
+    token = 'hardcoded-client-token-unsafe';
 
     handleInput(event) {
         this.userInput = event.target.value;
@@ -10,5 +11,24 @@ export default class VulnerableComponent extends LightningElement {
     get unsafeHtml() {
         // Vulnerable to XSS
         return '<div>' + this.userInput + '</div>';
+    }
+
+    renderedCallback() {
+        const host = this.template.querySelector('.output');
+        if (host) {
+            // DOM XSS via direct innerHTML assignment.
+            host.innerHTML = this.userInput;
+        }
+    }
+
+    runUserScript() {
+        // Code injection through eval.
+        // eslint-disable-next-line no-eval
+        return eval(this.userInput);
+    }
+
+    insecureRandomId() {
+        // Predictable randomness.
+        return Math.random().toString(36).substring(2);
     }
 }
